12101cdf0259a2d7c792190f38030d75e771406e
Matt Orrock <mattorrock@gmail.com>
2019-09-17 00:36:00 -0400

    Fixed profile page translations and verbose

--- users/models.py
+++ users/models.py
@@ -16,14 +16,14 @@ class Profile(models.Model):
     ]
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default_profile.jpg', upload_to='profile_pics', help_text=_("For the best looking profile picture, use a 1:1 size ratio. Images will be rescaled to 150x150 pixels!"), verbose_name=_('image'))
-    vk_link = models.CharField(max_length=150, help_text=_("Links your VK URL to your comments. You can optionally enable people to respond to your classified ads, via VK."), default="https://vk.com", blank=True, verbose_name=_('vk_link'))
-    twitter_link = models.CharField(max_length=150, help_text=_("Links your twitter URL to your comments. You can optionally enable people to respond to your classified ads, via twitter."), default="https://twitter.com", blank=True, verbose_name=_('twitter_link'))
-    instagram_link = models.CharField(max_length=150, help_text=_("Links your instagram URL to your comments. You can optionally enable people to respond to your classified ads, via instagram"), default="https://www.instagram.com", blank=True, verbose_name=_('instagram_link'))
-    facebook_link = models.CharField(max_length=150, help_text=_("Links your facebook URL to your comments. You can optionally enable people to respond to your classified ads, via facebook"), default="https://www.facebook.com", blank=True, verbose_name=_('facebook_link'))
-    language = models.CharField(max_length=30, choices=LANGUAGE, default=ENGLISH, help_text=_("(Blogs are written in English first.)"), verbose_name=_('language'))
-    subscribe_to_food_blogs = models.BooleanField(default=True, help_text=_('Do you wish to be notified with an email when a new food blog post is published?'), verbose_name=_('subscribe_to_food_blogs'))
-    subscribe_to_Life_in_Moscow_blogs = models.BooleanField(default=True, help_text=_('Do you wish to be notified with an email when a new Life in Moscow blog is published?'), verbose_name=_('subscribe_to_Life_in_Moscow_blogs'))
+    image = models.ImageField(default='default_profile.jpg', upload_to='profile_pics', help_text=_("For the best looking profile picture, use a 1:1 size ratio. Images will be rescaled to 150x150 pixels!"), verbose_name=_('Profile Image'))
+    vk_link = models.CharField(max_length=150, help_text=_("Links your VK URL to your comments. You can optionally enable people to respond to your classified ads, via VK."), default="https://vk.com", blank=True, verbose_name=_('Vk Link'))
+    twitter_link = models.CharField(max_length=150, help_text=_("Links your twitter URL to your comments. You can optionally enable people to respond to your classified ads, via twitter."), default="https://twitter.com", blank=True, verbose_name=_('Twitter Link'))
+    instagram_link = models.CharField(max_length=150, help_text=_("Links your instagram URL to your comments. You can optionally enable people to respond to your classified ads, via instagram"), default="https://www.instagram.com", blank=True, verbose_name=_('Instagram Link'))
+    facebook_link = models.CharField(max_length=150, help_text=_("Links your facebook URL to your comments. You can optionally enable people to respond to your classified ads, via facebook"), default="https://www.facebook.com", blank=True, verbose_name=_('Facebook Link'))
+    language = models.CharField(max_length=30, choices=LANGUAGE, default=ENGLISH, help_text=_("(Blogs are written in English first.)"), verbose_name=_('Language'))
+    subscribe_to_food_blogs = models.BooleanField(default=True, help_text=_('Do you wish to be notified with an email when a new food blog post is published?'), verbose_name=_('Subscribe to food blogs'))
+    subscribe_to_Life_in_Moscow_blogs = models.BooleanField(default=True, help_text=_('Do you wish to be notified with an email when a new Life in Moscow blog is published?'), verbose_name=_('Subscribe to Life in Moscow blogs'))
 
     def __str__(self):
         return f'{self.user.username} Profile'
